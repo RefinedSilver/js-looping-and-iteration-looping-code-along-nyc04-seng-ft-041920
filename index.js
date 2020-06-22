deaf6007e8ec16ce94d1a6f04b181a5fe7c4e4cc
github username <github email address>
2020-06-22 00:24:07 -0400

    Created  a for loop

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(n, eN){
+  for (let i = n.length-1 ; i >= 0  ;i-- ) {
+    console.log(`Thank you, ${n[i]}, for the wonderful ${eN} gift!`)
+  }
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(n, eN){
+  for (let i = n.length-1 ; i >= 0  ;i-- ) {
+    console.log(`Thank you, ${n[i]}, for the wonderful ${eN} gift!`)
+  }
+}
+countDown(["Ada", "Brendan", "Ali"], "birthday");
